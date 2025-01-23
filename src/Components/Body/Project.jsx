8131870fbfca59dfc30b672e9cd73072aa5c19b8
Phoolchand Kumawat <pk1753842@gmail.com>
2025-01-23 12:21:59 +0530

    made changes to the Project.jsx file

--- src/Components/Body/Project.jsx
+++ src/Components/Body/Project.jsx
@@ -7,59 +7,67 @@ function Project() {
 
 
   return (
-    <div className='w-full h-screen bg-black text-white'>
+    <div className='w-full h-screen bg-black text-white sm:pt-10 pt-52'>
         <div className="w-5/6 m-auto sm:flex h-full">
         <div className="left w-1/2 h-4/5 my-auto flex">
             <div className="line w-5 h-full">
                 <div className='w-[2px] h-full bg-gray-900'></div>
             </div>
-            <div className="content w-[calc(100%-20px)] grid relative">
+            <div className="content w-[calc(100%-20px)] grid relative h-full">
                 <div className="project flex gap-10 relative -left-[29px] mt-5">
                     <div className="point w-5 h-5 bg-gray-900 rounded-full absolute top-[30%] left-0"></div>
                     <div className="textpro cursor-pointer ml-16" onMouseEnter={()=>setMouser(projectImages2)} onMouseLeave={()=>setMouser(false)}>
-                        <h2 className=''>Complete E-Store With Baas As Backend</h2>
-                        <p className='flex gap-5'>
-                            <span>React</span>
-                            <span>Appwrite</span>
-                            <span>Tailwind</span>
-                            <span>Redux Toolkit</span>
-                        </p>
+                        <a href="https://freedemostore.netlify.app" target="_blank">
+                            <h2 className=''>Complete E-Store With Baas As Backend</h2>
+                            <p className='flex gap-5'>
+                                <span>React</span>
+                                <span>Appwrite</span>
+                                <span>Tailwind</span>
+                                <span>Redux Toolkit</span>
+                            </p>
+                        </a>
                         
                     </div>
                 </div>
                 <div className="project flex gap-10 relative -left-[29px] mt-5" onMouseEnter={()=>setMouser(projectImages4)} onMouseLeave={()=>setMouser(false)}>
                     <div className="point w-5 h-5 bg-gray-900 rounded-full absolute top-[30%] left-0"></div>
                     <div className="textpro cursor-pointer ml-16">
-                        <h2 className=''>A Open Word Chat App</h2>
-                        <p className='flex gap-5'>
-                            <span>React</span>
-                            <span>Redux Toolkit</span>
-                            <span>Appwrite</span>
-                            <span>Tailwind</span>
-                        </p>
+                        <a href="https://openworldchat.netlify.app" target="_blank">
+                            <h2 className=''>A Open Word Chat App</h2>
+                            <p className='flex gap-5'>
+                                <span>React</span>
+                                <span>Redux Toolkit</span>
+                                <span>Appwrite</span>
+                                <span>Tailwind</span>
+                            </p>
+                        </a>
                     </div>
                 </div>
                 <div className="project flex gap-10 relative -left-[29px] mt-5" onMouseEnter={()=>setMouser(projectImages5)} onMouseLeave={()=>setMouser(false)}>
                     <div className="point w-5 h-5 bg-gray-900 rounded-full absolute top-[30%] left-0"></div>
                     <div className="textpro cursor-pointer ml-16">
-                        <h2 className=''>Zelt Website Clone</h2>
-                        <p className='flex gap-5'>
-                            <span>Html</span>
-                            <span>Css</span>
-                            <span>Java Script</span>
-                        </p>
+                        <a href="https://phoolchandkumawat.github.io/zelt-clone" target="_blank">
+                            <h2 className=''>Zelt Website Clone</h2>
+                            <p className='flex gap-5'>
+                                <span>Html</span>
+                                <span>Css</span>
+                                <span>Java Script</span>
+                            </p>
+                        </a>
                     </div>
                 </div>
                 <div className="project flex gap-10 relative -left-[29px] mt-5" onMouseEnter={()=>setMouser(projectImages3)} onMouseLeave={()=>setMouser(false)}>
                     <div className="point w-5 h-5 bg-gray-900 rounded-full absolute top-[30%] left-0"></div>
                     <div className="textpro cursor-pointer ml-16">
-                        <h2 className=''>Food Website Clone{"(Eat-Curius)"}</h2>
-                        <p className='flex gap-5'>
-                            <span>React</span>
-                            <span>Tailwind</span>
-                            <span>Framor Motion</span>
-                            <span>Swiper Js</span>
-                        </p>
+                        <a href="https://eatcurius.netlify.app" target="_blank">
+                            <h2 className=''>Food Website Clone{"(Eat-Curius)"}</h2>
+                            <p className='flex gap-5'>
+                                <span>React</span>
+                                <span>Tailwind</span>
+                                <span>Framor Motion</span>
+                                <span>Swiper Js</span>
+                            </p>
+                        </a>
                     </div>
                 </div>
             </div>
